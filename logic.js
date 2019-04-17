7340344f0e3b0dfb872aceb6c8f9718d224a7f1b
Matt <matt@mwjohnston.com>
2019-04-17 13:21:52 -0700

    scaled the magnitute ranges on the legend and markers down to better reflect the majority of earthquakes in dataset

--- logic.js
+++ logic.js
@@ -10,18 +10,17 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson").then(function(data) {
   var data = data["features"];
   console.log(data);
-
+  //loops through data to create colored circles based off magnitude
   for (var i = 0; i < data.length; i++) {
     var mag = data[i]["properties"]["mag"];
     var color = "";
     var coordinates = data[i]['geometry']['coordinates'];
     var place = data[i]["properties"]["place"];
 
-    if (mag < 2.9) {
+    if (mag < 3) {
       color = "Green";
     }else if (mag < 3.5) {
       color = "GreenYellow";
@@ -29,10 +28,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geo
     else if (mag < 4) {
       color = "Yellow";
     }
-    else if (mag < 6) {
+    else if (mag < 4.5) {
       color = "Orange";
     }
-    else if (mag < 7) {
+    else if (mag < 5) {
       color = "OrangeRed";
     }
     else if (mag < 10) {
